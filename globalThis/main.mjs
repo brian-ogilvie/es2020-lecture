92ed93a81f5428d57c085edebe6169678a05ca3e
Brian Ogilvie <brian@brianogilviedesigns.com>
2020-09-13 23:06:20 -0400

    refactor globalThis main.mjs

--- globalThis/main.mjs
+++ globalThis/main.mjs
@@ -3,13 +3,12 @@ import './GlobalModule.js';
 function runInBrowser() {
   window.logGlobalObject();
 }
+const browserButton = document.getElementById('browser-button');
+browserButton.addEventListener('click', runInBrowser);
 
 function runInWorker() {
   const worker = new Worker('./worker.mjs');
   worker.postMessage('logGlobalObject');
 }
-
-const browserButton = document.getElementById('browser-button');
 const workerButton = document.getElementById('worker-button');
-browserButton.addEventListener('click', runInBrowser);
 workerButton.addEventListener('click', runInWorker);
